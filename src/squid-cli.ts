a9a0f5b0ce558ac1054396b6eed090c461440e29
NilsRamstoeck <nilsramstoeck@gmail.com>
2023-05-16 12:59:59 +0200

    added start/stop to cli

--- src/squid-cli.ts
+++ src/squid-cli.ts
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 
+import { spawn } from "child_process";
 import { program } from "commander";
+import { createSocket } from "dgram";
 import { existsSync } from "fs";
 import { rm } from "fs/promises";
 import nodemon from "nodemon";
+import path from "path";
 import { getComponentContext, getSquidContext } from "./modules/compile";
-import { server } from "./modules/server";
+// import { server } from "./modules/server";
 
 import type * as Types from "./types";
-export {Types};
+export { Types };
 
 async function getContext() {
   if (existsSync('./build'))
@@ -36,7 +39,7 @@ async function getContext() {
     rebuild,
     watch,
     dispose
-  }
+  };
 }
 
 program
@@ -56,7 +59,7 @@ program
   .action(async () => {
 
     const { rebuild, watch } = await getContext();
-    
+
     await rebuild();
     await watch();
 
@@ -71,7 +74,40 @@ program
   .command('start')
   .description('Starts the Squid server')
   .action(() => {
-    server.start();
+    const ipcSocket = createSocket('udp4');
+    const port = Number.parseInt(process.env.SQUID_PORT ?? '0') || 3000;
+    //Process must NOT inherit stdio. This causes the gitlab CI/CD pipeline to get stuck (because why not?)
+    //even after the CLI process terminates (prolly cause it waits for stdio to be free again?)
+    const serverProcess = spawn('node', [path.resolve(process.cwd(), 'build/squid-server.mjs')], {
+      cwd: process.cwd(),
+      detached: true
+    });
+
+    serverProcess.on('exit', () => process.exit());
+
+    ipcSocket.on("message", (msg) => {
+      if (msg.toString('utf-8') == 'started')
+        process.exit();
+    });
+
+    ipcSocket.bind(port - 1, 'localhost');
+
+    //We still gotta get the console output tho
+    serverProcess.stdout.on('data', data => process.stdout.write(data));
+    serverProcess.stderr.on('data', data => process.stderr.write(data));
+    serverProcess.unref();
+  });
+
+program
+  .command('stop')
+  .description('Stops the Squid server')
+  .action(() => {
+    const ipcSocket = createSocket('udp4');
+    const port = Number.parseInt(process.env.EXPRESS_PORT ?? '0') || 3000;
+    ipcSocket.send('exit', port + 1, 'localhost', (err) => {
+      console.log(err ?? 'Stopped');
+      process.exit();
+    });
   });
 
 program.parse();
--- src/squid-server.ts
+++ src/squid-server.ts
@@ -1,3 +1,24 @@
-import { server } from "./modules/server";
+import express from "@/modules/express";
+import { createSocket } from 'dgram';
 
-server.start();
+const app = express();
+
+const port = Number.parseInt(process.env.SQUID_PORT ?? '0') || 3000;
+
+const ipcSocket = createSocket("udp4");
+
+// Catching the message event
+ipcSocket.on("message", function (msg) {
+
+  if (msg.toString('utf-8') == 'exit')
+    process.exit();
+
+});
+
+// Binding server with port
+ipcSocket.bind(port + 1, 'localhost');
+
+app.listen(port, () => {
+  console.log(`✅ Express server listening on port ${port}`);
+  ipcSocket.send('started', port - 1, 'localhost');
+});
