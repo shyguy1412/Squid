dd77712f4e868d30cdd3596270022dfed4a26709
Nils Ramstoeck <nilsramstoeck@gmail.com>
2023-09-06 14:31:21 +0100

    add command to generate new component

--- src/squid-cli.ts
+++ src/squid-cli.ts
@@ -8,6 +8,10 @@ import fs from "fs/promises";
 import { existsSync as fileExists } from "fs";
 import { getContext } from "./modules/compile";
 
+import PageTemplate from '@/templates/Page.txt';
+import ApiTemplate from '@/templates/ApiEndpoint.txt';
+import ComponentTemplate from '@/templates/Component.txt';
+
 (async () => {
   try {
     const package_json = JSON.parse((await fs.readFile('./package.json')).toString());
@@ -30,7 +34,7 @@ import { getContext } from "./modules/compile";
       try {
         await rebuild();
       } catch (e) { }
-      
+
       await dispose();
     });
 
@@ -67,5 +71,55 @@ import { getContext } from "./modules/compile";
       });
     });
 
+  program
+    .command('gen')
+    .argument('<type>', 'What component to generate. (comp, page, api)')
+    .argument('<path>', 'Where to generate the component')
+    .description('Generate new components, pages or API endpoints.')
+    .action(async (type, path) => {
+      if (!path) throw new Error('No path was given');
+      if (typeof path !== 'string') throw new Error('Invalid path: ' + path);
+      switch (type) {
+        case 'comp':
+          try {
+            const fullPath = './src/components/' + path;
+            await createDirectory(fullPath);
+            await fs.writeFile((fullPath).replace(/(.tsx)?$/, '.tsx'), ComponentTemplate.replace('%COMPONENT_NAME%', path.split('/').at(-1) ?? 'Component'), {});
+          } catch (e) {
+            console.log(e);
+            throw new Error('Invalid path');
+          }
+          break;
+        case 'page':
+          try {
+            const fullPath = './src/pages/' + path;
+            await createDirectory(fullPath);
+            await fs.writeFile((fullPath).replace(/(.tsx)?$/, '.tsx'), PageTemplate);
+          } catch (e) {
+            throw new Error('Invalid path');
+          }
+          break;
+        case 'api':
+          try {
+            const fullPath = './src/pages/' + path;
+            await createDirectory(fullPath);
+            await fs.writeFile((fullPath).replace(/(.ts)?$/, '.ts'), ApiTemplate);
+          } catch (e) {
+            throw new Error('Invalid path');
+          }
+          break;
+        default:
+          throw new Error('Invalid type: ' + type);
+      }
+    })
+    .options;
+
   program.parse();
 })();
+
+async function createDirectory(path: string) {
+  try {
+    await fs.mkdir(path.replace(/\/([^\/]+)\/?$/, ''), { recursive: true });
+  } catch (e) {
+  }
+}
