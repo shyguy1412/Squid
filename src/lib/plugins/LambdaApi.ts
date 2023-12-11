6ac1ce5613e2598ed8b9cca9f8ed27a3cce258e5
Nils Ramstoeck <nilsramstoeck@gmail.com>
2023-12-11 18:12:13 +0000

    add url to lambda exports

--- src/lib/plugins/LambdaApi.ts
+++ src/lib/plugins/LambdaApi.ts
@@ -4,12 +4,15 @@ import fs from 'fs/promises';
 import path from "path";
 
 const exportLambdaTemplate = `\
-export function %%NAME%%(options?:RequestInit) {
+export function %%FUNCTION%%(options) {
   return fetch('%%PATH%%', options);
 }
+export const %%FUNCTION_URL%% = '%%PATH%%';
 `;
 
-const moduleTemplate = `export function %%NAME%%(options: RequestInit): Promise<Response>;`;
+const moduleTemplate = `\
+export function %%FUNCTION%%(options?: RequestInit): Promise<Response>;
+export const %%FUNCTION_URL%%: '%%PATH%%';`;
 
 /**
  * Generates functions to easily use lambda functions
@@ -32,17 +35,20 @@ export const GenereateLambdaApi = (squidOptions: SquidOptions) => ({
         const lambdaPath = `${squidOptions.lambdaGateway}/function/${functionEndpoint}`;
 
         const exportStatement = exportLambdaTemplate
-          .replace('%%NAME%%', functionName)
-          .replace('%%PATH%%', lambdaPath.replace(/^(http(s)?:\/\/)?/, 'http$2://'));
+          .replaceAll('%%FUNCTION%%', functionName)
+          .replaceAll('%%FUNCTION_URL%%', `${functionName}Url`)
+          .replaceAll('%%PATH%%', lambdaPath.replace(/^(http(s)?:\/\/)?/, 'http$2://'));
 
         const moduleStatement = moduleTemplate
-          .replace('%%NAME%%', functionName);
+          .replaceAll('%%FUNCTION_URL%%', `${functionName}Url`)
+          .replaceAll('%%FUNCTION%%', functionName)
+          .replaceAll('%%PATH%%', lambdaPath.replace(/^(http(s)?:\/\/)?/, 'http$2://'));
 
         exportStatements.push(exportStatement);
         moduleStatements.push(moduleStatement);
       }
 
-      await fs.writeFile('./build/lambda.ts', exportStatements.join('\n'));
+      await fs.writeFile('./build/lambda.js', exportStatements.join('\n'));
       await fs.writeFile('./build/lambda.module.d.ts', `declare module 'squid-ssr/lambda' {\n${moduleStatements.join('\n')}\n}`);
     });
 
@@ -56,7 +62,7 @@ export const LambdaApiPlugin = ({
     pluginBuild.onResolve({ filter: /squid-ssr\/lambda/ }, async () => {
       // console.log(path.resolve('./build/lambda.ts'));
       return {
-        path: path.resolve('./build/lambda.ts')
+        path: path.resolve('./build/lambda.js')
       };
     });
   }
